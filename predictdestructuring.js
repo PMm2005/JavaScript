f4cb68b7774ff7b023f92fba6ef9c58ff93ba233
Mohamed Amin Amri <amrim893@gmail.com>
2024-09-12 17:50:00 +0200

    Predict Destructuring

---
+++ predictdestructuring.js
@@ -0,0 +1,30 @@
+//const cars = ['Tesla', 'Mercedes', 'Honda']
+//const [ randomCar ] = cars
+//const [ ,otherRandomCar ] = cars
+//Predict the output
+//console.log(randomCar)
+//console.log(otherRandomCar)
+//  !tesla mercedes
+
+//const employee = {
+//    name: 'Elon',
+//    age: 47,
+//    company: 'Tesla'
+//}
+//const { name: otherName } = employee;
+//Predict the output
+//console.log(name);
+//console.log(otherName);
+//!  name undifined
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
